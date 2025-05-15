3d07c75a6b1fd9e7fac43fb01f2b412d86ea57fb
Guilherme <83481058+45Guilherme@users.noreply.github.com>
2025-05-15 14:55:45 -0300

    Mudança no corpo do código

--- calculadora.cpp
+++ calculadora.cpp
@@ -1,4 +1,25 @@
 #include <iostream>
+#include <cmath>
 
+using namespace std;
+
+int main() {
+    double a,b, c, d;
+    double sum, sub, mul, div, mod, exp;
+    cout << "Welcome to the calculator program!" << endl;
+    cout << "Enter two numbers: ";
+    cin >> a >> b >> c >> d;
+    sum = a + b;
+    sub = a - b;
+    mul = a * b;
+    div = a / b;
+    mod = fmod(a, b);
+    exp = pow(a, b);
+
+    cout << "Sum = " << sum << endl;
+
+
+    return 0;
+}
 
 
